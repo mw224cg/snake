00be4fa4cb764c873b5485861f3e71d03a5dc13b
mw224cg <138625041+mw224cg@users.noreply.github.com>
2025-10-02 13:46:31 +0200

    Create rand.h

---
+++ rand.h
@@ -0,0 +1,11 @@
+// random.h
+#ifndef RANDOM_H
+#define RANDOM_H
+
+// Sätt startvärde (seed) för slumpgeneratorn
+void srand(unsigned int s);
+
+// Generera ett nytt slumptal (0–32767)
+int rand(void);
+
+#endif // RANDOM_H
